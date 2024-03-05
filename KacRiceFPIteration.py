b7ae35f1de938c16d3b5ae41ed4b1c02d5d3ca9b
IanJoffe <iijoffe@gmail.com>
2024-03-04 20:04:20 -0600

    ready for submission

--- KacRiceFPIteration.py
+++ KacRiceFPIteration.py
@@ -1,9 +1,18 @@
+"""
+Code to compute the annealed complexity, written by Ian Joffe
+From formula (31) of Landscape Complexity for the Empirical Risk of Generalized Linear Models
+    by Maillard, Ben Arous, and Biroli
+Please see attached report for details 
+"""
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 mc_iterations = 100
 alpha = 20
 eps = 1e-3
 
+# define phi as the quadratic for phase retrieval
 def phi(x):
     return x**2
 def dphi(x):
@@ -14,6 +23,9 @@ def ddphi(x):
 # monte carlo integral for the numerator of <func>
 def integral1_mc(func, lambda_0, lambda_1, g_re, g_im):
     total = 0
+    # Given my limited computation, I need to use the same seed for each MC integral
+    # or else I can't compare them or manually get their derivatives
+    np.random.seed(0)
     for i in range(mc_iterations):
         x = np.random.normal(0,1)
         Dx = func(x)
@@ -33,6 +45,7 @@ def integral1_mc(func, lambda_0, lambda_1, g_re, g_im):
 # monte carlo integral for the denominator of <func>
 def integral2_mc(func, lambda_0, lambda_1, g_re, g_im):
     total = 0
+    np.random.seed(0)
     for i in range(mc_iterations):
         x = np.random.normal(0,1)
         Dx = func(x)
@@ -54,6 +67,7 @@ def integral3_mc(func, lambda_0, lambda_1, g_re, g_im):
     lambda_2 = g_re
 
     total = 0
+    np.random.seed(0)
     for i in range(mc_iterations):
         x = np.random.normal(0,1)
         Dx = func(x)
@@ -68,10 +82,11 @@ def integral3_mc(func, lambda_0, lambda_1, g_re, g_im):
                 )
             )
         )
+    return total / mc_iterations
 
 # formula for the annealed complexity
 def annealed_complexity(func, l, lambda_0, lambda_1, g_re, g_im):
-    return -1 + np.log(alpha) - 2*alpha*np.log(alpha) + \
+    return 1/2*(-1 + np.log(alpha) - 2*alpha*np.log(alpha)) + \
     lambda_0 * l + \
     (1 + np.log(2))/2 + \
     1/2 * np.log(lambda_1) - \
@@ -82,34 +97,35 @@ def annealed_complexity(func, l, lambda_0, lambda_1, g_re, g_im):
 def fixed_point_update(func, init, iterations=100):
     # ensure the algorithm will converge by checking its derivative at init
     dfunc_init = np.abs((func(init + eps) - func(init - eps)) / (2*eps))
-    # assert dfunc_init < 1, "Improper initialization for fixed point iteration, df|x=" + str(dfunc_init)
+    assert dfunc_init < 1, "Improper initialization for fixed point iteration, df|x=" + str(dfunc_init)
     param = init
     for j in range(iterations):
         param = func(param) + param
     return param
 
 def update_l(l_init, lambda_0, lambda_1, g_re, g_im, iterations):
-    # use fixed point iteration to update l, with other variables fixed
-    l_opt = l_init
-    for i in range(iterations):
-        l_opt = fixed_point_update(
-            lambda l: integral1_mc(phi, lambda_0, lambda_1, g_re, g_im) / integral2_mc(phi, lambda_0, lambda_1, g_re, g_im) - l,
-            l_opt
-        )
-    return l_opt
+    # The paper recommends treating l as fixed while iterating over other variables
+    # This updates l according only to its formula, not iteratively
+    # print(l_init)
+    return integral1_mc(phi, lambda_0, lambda_1, g_re, g_im) / integral2_mc(phi, lambda_0, lambda_1, g_re, g_im)
 
 def update_extr(l, lambda_0_init, lambda_1_init, g_re_init, g_im_init, iterations):
     # use fixed point iteration to update all variables other than l, with l fixed
     lambda_0_opt, lambda_1_opt, g_re_opt, g_im_opt = lambda_0_init, lambda_1_init, g_re_init, g_im_init
     for i in range(iterations):
+        # print("Lambda_0 ", lambda_0_opt)
         lambda_0_opt = fixed_point_update(
             lambda lambda_0: integral1_mc(phi, lambda_0, lambda_1_opt, g_re_opt, g_im_opt) / integral2_mc(phi, lambda_0, lambda_1_opt, g_re_opt, g_im_opt) - l,
             lambda_0_opt
         )
+        # print("Lambda_0 Updated ", lambda_0_opt)
+        # print("Lambda_1 ", lambda_1_opt)
         lambda_1_opt = fixed_point_update(
-            lambda lambda_1: 2*lambda_1*integral1_mc(lambda x: dphi(x)**2, lambda_0_opt, lambda_1, g_re_opt, g_im_opt) / integral2_mc(lambda x: dphi(x)**2, lambda_0_opt, lambda_1, g_re_opt, g_im_opt) - 1,
+            lambda lambda_1: integral1_mc(lambda x: dphi(x)**2, lambda_0_opt, lambda_1, g_re_opt, g_im_opt) / integral2_mc(lambda x: dphi(x)**2, lambda_0_opt, lambda_1, g_re_opt, g_im_opt) - 1/(2*lambda_1),
             lambda_1_opt
         )
+        # print("Lambda_1 Updated ", lambda_1_opt)
+        # print("g_re ", g_re_opt)
         g_re_opt = fixed_point_update(
             lambda g_re: integral1_mc(
                 lambda x: x*dphi(x) - (alpha*ddphi(x)*(alpha + ddphi(x)*g_re))/((alpha + ddphi(x)*g_re)**2 + (alpha + ddphi(x)*g_im_opt)**2),
@@ -117,9 +133,11 @@ def update_extr(l, lambda_0_init, lambda_1_init, g_re_init, g_im_init, iteration
             ) / integral2_mc(
                 lambda x: x*dphi(x) - (alpha*ddphi(x)*(alpha + ddphi(x)*g_re))/((alpha + ddphi(x)*g_re)**2 + (alpha + ddphi(x)*g_im_opt)**2),
                 lambda_0_opt, lambda_1_opt, g_re, g_im_opt
-            ) + g_re / (g_re**2 + g_im_opt**2), 
+            ) * (-1)/(g_re / (g_re**2 + g_im_opt**2)) - 1, 
             g_re_opt
         )
+        # print("g_re Updated ", g_re_opt)
+        # print("g_im ", g_im_opt)
         g_im_opt = fixed_point_update(
             lambda g_im: -integral1_mc(
                 lambda x: (alpha*ddphi(x)**2 * g_im) / ((alpha + ddphi(x)*g_re_opt)**2 + (alpha + ddphi(x)*g_im)**2),
@@ -130,13 +148,20 @@ def update_extr(l, lambda_0_init, lambda_1_init, g_re_init, g_im_init, iteration
             ) + g_im/(g_im**2 + g_re_opt**2) - eps,
             g_im_opt
         )
+        # print("g_im Updated ", g_im_opt)
     return lambda_0_opt, lambda_1_opt, g_re_opt, g_im_opt
 
 def solve_optimization(l_main, lambda_0_main, lambda_1_main, g_re_main, g_im_main, l_iterations, extr_iterations, total_iterations):
     # get the optimized annealed complexity for the initializations given as parameters
+    all_complexities = [annealed_complexity(phi, l_main, lambda_0_main, lambda_1_main, g_re_main, g_im_main)] # used to draw optimization trajectory curve
     for iter in range(total_iterations):
         lambda_0_main, lambda_1_main, g_re_main, g_im_main = update_extr(l_main, lambda_0_main, lambda_1_main, g_re_main, g_im_main, extr_iterations)
         l_main = update_l(l_main, lambda_0_main, lambda_1_main, g_re_main, g_im_main, l_iterations)
-    return annealed_complexity(lambda_0_main, lambda_1_main, g_re_main, g_im_main)
+        all_complexities.append(annealed_complexity(phi, l_main, lambda_0_main, lambda_1_main, g_re_main, g_im_main))
+    return annealed_complexity(phi, l_main, lambda_0_main, lambda_1_main, g_re_main, g_im_main)
 
-solve_optimization(-1, -1, -1, -1, -1, l_iterations=10, extr_iterations=10, total_iterations=100)
+# These initializations are not especially principled, 
+# but when you use them the FP iteration algorithm gets the necessary derivatives with magnitidue < 1
+# and the resulting annealed complexity matches the results of the paper, so I believe they are valid.
+# In the future I hope to further explore the space of initial parameters to confirm my results. 
+solve_optimization(1, 100, 100, 100, 1, l_iterations=1, extr_iterations=1, total_iterations=10000)
